2e0a038ce9605a4268c49a666ac8776ad1d19b4c
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2021-10-19 14:34:20 -0300

    Custom Attribute Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { DishService } from './services/dish.service';
 import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 import { PromotionService } from './services/promotion.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { PromotionService } from './services/promotion.service';
     HeaderComponent,
     HomeComponent,
     MenuComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
